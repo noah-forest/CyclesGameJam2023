3cc38e9c52fdb3922da15b164c04907a7c9d68f1
ZTattempt <62923868+ZTattempt@users.noreply.github.com>
2023-08-03 15:35:00 -0500

    Added playlist of music

--- Assets/Scripts/MainMenu/GameMusicPlayer.cs
+++ Assets/Scripts/MainMenu/GameMusicPlayer.cs
@@ -2,27 +2,82 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using Random = UnityEngine.Random;
 
 public class GameMusicPlayer : MonoBehaviour
 {
-    private static GameMusicPlayer instance;
+    public AudioClip[] songs;
+    private AudioSource _audioSource;
+    [SerializeField] private float _songsPlayed;
+    [SerializeField] private bool[] _beenPlayed;
+    
+    #region singleton
 
-    public static GameMusicPlayer Instance
-    {
-        get { return instance; }
-    }
-
+    private static GameMusicPlayer Instance { get; set; }
+    
     private void Awake()
     {
-        if (instance != null && instance != this)
+        if (Instance != null && Instance != this)
         {
             Destroy(this.gameObject);
             return;
         }
         else
         {
-            instance = this;
+            Instance = this;
         }
         DontDestroyOnLoad(this.gameObject);
+    }
+
+    #endregion
+
+    private void Start()
+    {
+        _audioSource = GetComponent<AudioSource>();
+        _beenPlayed = new bool[songs.Length];
+    }
+
+    private void Update()
+    {
+        if (SceneManager.GetActiveScene().name == "MainMenu") return;
+        
+        if (!_audioSource.isPlaying)
+        {
+            ChangeSong(Random.Range(0, songs.Length));
+        }
+
+        ResetShuffle();
+    }
+
+    private void ChangeSong(int songPicked)
+    {
+        if (!_beenPlayed[songPicked])
+        {
+            _songsPlayed++;
+            _beenPlayed[songPicked] = true;
+            _audioSource.clip = songs[songPicked];
+            _audioSource.Play();
+        } else
+            _audioSource.Stop();
+    }
+
+    private void ResetShuffle()
+    {
+        if (_songsPlayed == songs.Length)
+        {
+            _songsPlayed = 0;
+            for (var i = 0; i < songs.Length; i++)
+            {
+                if (i == songs.Length)
+                {
+                    break;
+                }
+                else
+                {
+                    _beenPlayed[i] = false;
+                }
+            }
+        }
     }
 }
